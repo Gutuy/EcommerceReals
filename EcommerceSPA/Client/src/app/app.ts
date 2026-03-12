8672e68aab6784486f1cdc5d8d4f866a3a0dd43f
gutu james <gutu j>
2026-03-12 11:51:49 +0300

    angular setup

--- EcommerceSPA/Client/src/app/app.ts
+++ EcommerceSPA/Client/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: []
 })
 export class App {
-  protected readonly title = signal('Client');
+  protected readonly title = 'Skinet';
 }
